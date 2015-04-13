b82c54410c76c34f7e32ce2e56dd5540114bfaf3
MCOlivera <mervin.c.olivera@gmail.com>
2015-04-13 19:18:26 +0000

    Fixed login controller.

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,15 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-    class Login extends CI_Controller{
+    class Login extends CI_Controller {
 
-        function __construct(){
+        function __construct()
+        {
             parent::__construct();
         }
 
-        function index(){
+        function index()
+        {
             $this->load->helper("url");
-			$this->load->view('login_view');
-			$this->load->view('components/footer.php');
+            $this->load->helper('form');
+            $this->load->view('login_view.php');
         }
 
     }
